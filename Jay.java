c5a1d44e7c40386240697ac49720623dc68ff024
Jay <88573059+Jay-Phoenix@users.noreply.github.com>
2023-04-05 00:15:05 +0530

    Add files via upload

---
+++ Jay.java
@@ -0,0 +1,20 @@
+//purple - java reserved key words
+
+package week1.day1;
+
+// Public class can be accessible anywhre inside the project
+public class Jay {
+
+	// main method
+	// we cant execute java code without main method
+	// Format - Ctrl + Shift + F
+	// To execute - Ctrl + F11
+
+	public static void main(String[] args) {
+
+		System.out.println("How are you doing?");
+		// TODO Auto-generated method stub
+
+	}
+
+}
